af2df266a5f230513b57a2c827bc1cbdebff4046
Mani Bharathi <mani.bharathi.tech03#gmail.com>
2026-06-22 01:00:07 +0530

    Added Two Sum screenshots and updated solution

--- Arrays/Two_Sum/0001_Two_Sum.py
+++ Arrays/Two_Sum/0001_Two_Sum.py
@@ -3,23 +3,23 @@
 # Space Complexity: O(1)
 
 class Solution:
-    def twoSum(self, nums , target):
+    def twoSum(self , nums , target):
         for i in range(len(nums)):
-            for j in range(j+1, len(nums)):
+            for j in range(i+1 , len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i, j]
+                    return [i,j]
                 
 
-
- #Approach 2: HashMap
+# Approach 2: HashMap
 # Time Complexity: O(n)
 # Space Complexity: O(n)
 
 class Solution:
-    def twoSum(Self, nums, target):
+    def twoSum(self, nums , target):
         hashmap = {}
         for i , num in enumerate(nums):
             complement = target - num
             if complement in hashmap:
-                return[hashmap[complement], i]
+                return[hashmap[complement] , i]
             hashmap[num] = i
+            
